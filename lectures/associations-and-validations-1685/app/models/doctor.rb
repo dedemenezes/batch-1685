97dec5d2d5d3b68fe0120289be68dd1de895f75e
Andre Menezes <djdedemenezes@gmail.com>
2024-05-10 11:01:23 -0300

    add new comments

--- lectures/associations-and-validations-1685/app/models/doctor.rb
+++ lectures/associations-and-validations-1685/app/models/doctor.rb
@@ -1,11 +1,19 @@
 class Doctor < ActiveRecord::Base
+  # associations
   has_many :interns
   # creates a method that retrieves ALL the interns
+  # e.g. doctor.interns
+
   has_many :consultations
   # creates a method that retrieves ALL the consultation
+  # e.g. doctor.consultations
+
   has_many :patients, through: :consultations
   # creates a method that retrieves ALL the patients
+  # e.g. doctor.patients
 
+  # validations
+  # validates :column_name, validation_name: value
   validates :last_name, presence: true # cannot be empty
   # validates :last_name, uniqueness: true
   validates :last_name, uniqueness: { scope: :first_name }
