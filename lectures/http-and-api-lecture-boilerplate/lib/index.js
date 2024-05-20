3942025a44020c252b333888d94f1786696a1ae3
Andre Menezes <djdedemenezes@gmail.com>
2024-05-20 11:10:35 -0300

    add lecture code

--- lectures/http-and-api-lecture-boilerplate/lib/index.js
+++ lectures/http-and-api-lecture-boilerplate/lib/index.js
@@ -1,2 +1,36 @@
-// TODO - Fetch an activity with the Bored API - Let's psuedocode!
+// Random activity from array ===== //
+const activities = ['Study JS', 'Do flahscards']
+
+// // 1. Select the button
+const btn = document.querySelector('.btn');
+const header = document.getElementById('activity')
+
+// // console.log(btn);
+// // 2. Add an event listener
+// btn.addEventListener('click', (event) => {
+//   // When the event happens
+//   // 3. choose a random activity inside the activities
+//   const randomIndex = Math.floor(Math.random() * 2)
+//   const activity = activities[randomIndex]
+//   // 4. Diplsay on the page
+//   // 4.1 Select the element where you want to display
+//   // 4.2 Insert the activity in the page
+//   header.innerHTML = activity
+// })
+
+// FETCH - Get random activity from API ===== //
 const url = 'https://www.boredapi.com/api/activity/'
+
+const insertActivity = (data) => {
+  // USE THE DATA
+  console.log(data.activity)
+  header.innerText = data.activity
+}
+
+// console.log(btn);
+// 2. Add an event listener
+btn.addEventListener('click', (event) => {
+  fetch(url) // make the HTTP request
+    .then(response => response.json()) // Parsing the response
+    .then(data => insertActivity(data))
+})
